08d33bf6d3469208f4af2f2ba7a405e9e4c36619
Ilona Tomkowicz <itomkowicz@microsoft.com>
2023-09-15 09:38:59 +0200

    Init

---
+++ zoo/src/lib.rs
@@ -0,0 +1,26 @@
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+cargo_component_bindings::generate!({
+    implementor: Greeter,
+});
+
+use bindings::exports::zoo::city::greeter::Guest;
+use bindings::zoo::city::city::{send_message, CityInfo};
+
+struct Greeter;
+
+impl Guest for Greeter {
+    fn run(info: CityInfo) {
+        if ((info.budget as f64) / (info.head_count as f64)) > 100.0 {
+            send_message(&format!(
+                "Welcome to {}, we invite you for a drink!",
+                info.name
+            ));
+        } else if info.head_count > 1_000_000 {
+            send_message(&format!("Welcome to {} mega polis!", info.name));
+        } else {
+            send_message(&format!("Welcome to {}!", info.name));
+        }
+    }
+}
---
+++ zoo/zoo-test.mjs
@@ -0,0 +1,38 @@
+import { instantiate } from "./target/js-jco/zoo.js"
+
+const isNode = typeof process !== 'undefined' && process.versions && process.versions.node;
+let _fs;
+async function fetchCompile(url) {
+    let self = import.meta.url.substring("file://".length);
+    if (self.indexOf(":") === 2) {
+        self = self.substring(1);
+    }
+    const u2 = self.substring(0, self.lastIndexOf("/"))+"/target/js-jco/"+url;
+    if (isNode) {
+        _fs = _fs || await import('fs/promises');
+        return WebAssembly.compile(await _fs.readFile(u2));
+    }
+    return fetch(u2).then(WebAssembly.compileStreaming);
+}
+
+const expectdMessage="Welcome to Prague, we invite you for a drink!";
+let actualMessage;
+const imports = {
+    'zoo:city/city': {
+        sendMessage: (message) => {
+            actualMessage = message;
+            console.log(message);
+        }
+    }
+}
+const component = await instantiate(fetchCompile, imports, WebAssembly.instantiate);
+const exports = component['greeter'];
+exports.run({
+    name: "Prague",
+    headCount: 1000000,
+    budget: BigInt(200000000)
+});
+
+if (actualMessage !== expectdMessage) {
+    throw new Error(`sendMessage: expected "${expectdMessage}" actual "${actualMessage}"`);
+}
