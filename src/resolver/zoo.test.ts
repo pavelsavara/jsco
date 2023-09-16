6941cf2c458087c8d7d147ae3ef83ac374b8e9f6
Pavel Savara <pavel.savara@gmail.com>
2023-09-17 01:31:44 +0200

    zoo failing in resolver (#50)

---
+++ src/resolver/zoo.test.ts
@@ -0,0 +1,60 @@
+import { instantiateComponent } from './index';
+import { setConfiguration } from '../utils/assert';
+import type { ZooFoodFood } from '../../tests/zoo-food-food.d.ts';
+import type { ZooFoodEater } from '../../tests/zoo-food-eater.d.ts';
+
+type TFeed = typeof ZooFoodEater.feed
+type TZooFoodFood = typeof ZooFoodFood
+
+setConfiguration('Debug');
+
+describe('resolver hello', () => {
+    test.failing('component zoo.wasm could run', async () => {
+        let actualMessage: string = undefined as any;
+        let actualFood: any = undefined as any;
+
+        const zooFood: TZooFoodFood = {
+            hideFood: (food, message) => {
+                actualMessage = message;
+                actualFood = food;
+            },
+            consumeFood: (foodinfo, packageinfo, message) => {
+                actualMessage = message;
+            },
+            openPackage: (packageinfo, message) => {
+                actualMessage = message;
+            },
+            trashPackage: (sealingstate, message) => {
+                actualMessage = message;
+            }
+        };
+        const imports = {
+            'zoo:food/food': zooFood
+        };
+
+        const instance = await instantiateComponent('./zoo/wasm/zoo.wasm', imports);
+        const feed = instance.exports['zoo:food/eater'].feed as TFeed;
+
+        feed({
+            name: 'apple',
+            isoCode: 'cz',
+            weight: 1,
+            healthy: true,
+            calories: 1n,
+            cost: 1,
+            rating: 1,
+            pieces: 1,
+            shelfTemperature: [1, 1],
+            cookTimeInMinutes: 1,
+        }, {
+            nutrition: {
+                percentage: 1,
+                nutritionType: 'carbohyrdate',
+            },
+            material: { tag: 'plastic-bag' },
+            sealing: 'closed',
+        });
+
+        expect(actualMessage).toBe('Welcome to Prague, we invite you for a drink!');
+    });
+});
