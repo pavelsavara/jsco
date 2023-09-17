41dbef15c66554554b64a4e7129a96525abc65a4
pavelsavara <pavel.savara@gmail.com>
2023-09-17 12:36:04 +0200

    make it work

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # jsco demo
 
-
 See [project page](https://github.com/pavelsavara/jsco)
--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 import { createComponent } from './@pavelsavara/jsco/index.js';
 
-function sendMessage(message) {
-    console.log(message);
-}
-
 async function createGreeter(sendMessage) {
     // this is url of your component
     // `hello.wasm` sample was built using `npm run build:hello`
@@ -34,11 +30,37 @@ function runGreeter(cityInfo){
     greeter.run(cityInfo);
 }
 
-export function onClick(){
-    console.log('onClick');
-    /*runGreeter({
-        name: 'Prague',
-        headCount: 100,
-        budget: 0n,
-    });*/
+function onClick(event){
+    console.log('onClick', event);
+
+    const name = cityInput.value;
+    const headCount = parseInt(headCountInput.value);
+    const budget = BigInt(budgetInput.value);
+
+    runGreeter({
+        name,
+        headCount,
+        budget,
+    });
 }
+
+let dropDots = true;
+
+function sendMessage(message) {
+    console.log(message);
+    
+    if(dropDots){
+        messages.innerText = '';
+        dropDots = false;
+    }
+
+    const div = document.createElement('div');
+    div.innerText = message;
+    messages.appendChild(div);
+}
+
+document.querySelector('#say-hi').addEventListener('click', onClick)
+const cityInput = document.querySelector('#city');
+const headCountInput = document.querySelector('#headCount');
+const budgetInput = document.querySelector('#budget');
+const messages = document.querySelector('#messages');
